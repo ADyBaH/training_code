7c32b2022f5fb6a0222fca758614d1597552c0ad
ADyBaH <Brandon4@mail.ru>
2022-10-08 16:25:25 +0300

    feat: add change in tryCatchJson

--- learn_javascript_ru/error_processing/tryCatchJson.js
+++ learn_javascript_ru/error_processing/tryCatchJson.js
@@ -1,13 +1,26 @@
-let json = '{"name":, "age": 30}';
+let json = '{"age": 30}';
 
 
 try {
+
   let user = JSON.parse(json);
+
+  if(!user.name) {
+    // Кастомная ошибка:
+    throw new SyntaxError("Данные неполные: нет имени")
+  }
+
+  blabla();
 
   console.log(user.name);
   console.log(user.age);
 } catch (e) {
-  console.log("Error"); // text
+
+  if(e.name == "SyntaxError") {
+    console.log("JSON Error: " + e.message);
+  } else {
+    throw e; // исключение
+  }
   console.log(e.name);  // Name Error
   console.log(e.message)  // why and position
 }
