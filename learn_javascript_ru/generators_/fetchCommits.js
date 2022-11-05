3d418557c9f0bb85c46e2b8993f23b2f5893f263
ADyBaH <Brandon4@mail.ru>
2022-11-05 12:26:26 +0300

    feat: add learned fetchCommits.js

---
+++ learn_javascript_ru/generators_/fetchCommits.js
@@ -0,0 +1,34 @@
+let repo = 'javascript-tutorial/en.javascript.info';
+
+
+async function* fetchCommits(repo) {
+  let url = `https://api.github.com/repos/${repo}/commits`;
+
+  while(url) {
+    const response = await fetch(url, {
+      headers: {'User-Agent': 'Our script'},
+    });
+
+    const body = await response.json();
+
+    let nextPage = response.headers.get('Link').match(/<(.*?)>; rel="next"/);
+    nextPage = nextPage && nextPage[1];
+
+    url = nextPage;
+
+    for(let commit of body) {
+      yield commit;
+    }
+  }
+}
+
+(async () => {
+
+  let count = 0;
+  for await (let commit of fetchCommits(repo)) {
+    console.log(commit.author.login);
+
+    if(++count == 100) break;
+  }
+
+})();
