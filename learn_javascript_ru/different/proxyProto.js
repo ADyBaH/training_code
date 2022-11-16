c186815771678a332558098497c65ba013d37561
ADyBaH <Brandon4@mail.ru>
2022-11-16 13:40:26 +0300

    feat: add learned proxyProto.js

---
+++ learn_javascript_ru/different/proxyProto.js
@@ -0,0 +1,21 @@
+let user = {
+  _name: "Гость",
+  get name() {
+    return this._name;
+  }
+};
+
+let userProxy = new Proxy (user, {
+  get(target, prop, receiver) {
+    return Reflect.get(target, prop, receiver);
+    // return target[prop];
+  }
+});
+
+let admin = {
+  __proto__: userProxy,
+  _name: "Админ",
+};
+
+console.log(admin.name);
+console.log(userProxy.name);
