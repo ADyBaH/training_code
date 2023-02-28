d735b0e24e088f991d6e7c64b2718cf3d7e0a3e7
ADyBaH <Brandon4@mail.ru>
2023-02-28 13:42:42 +0300

    feat: add solution find HTML color  to regExTraining.js

--- learn_javascript_ru/chapter_3/regularExpression/regExTraining.js
+++ learn_javascript_ru/chapter_3/regularExpression/regExTraining.js
@@ -15,3 +15,12 @@ log( "Завтрак в 09:00. Ужин в 21-30".match(regexp1) ); // 09:00, 21
 
 let regexp2 = /[.]{1,}/g;
 log( "Привет!... Как дела?.....".match(regexp2) ); // ..., .....
+
+/* Напишите регулярное выражение, которое ищет HTML-цвета в формате #ABCDEF: 
+первым идёт символ #, и потом – 6 шестнадцатеричных символов. */
+
+let regexp = /#[\d\w][\d\w][\d\w][\d\w][\d\w][\d\w][\s;]/g
+// better solution: /#[a-f0-9]{6}\b/gi
+let str = "color:#121212; background-color:#AA00ef bad-colors:f#fddee #fd2 #12345678";
+
+alert( str.match(regexp) )  // #121212,#AA00ef
