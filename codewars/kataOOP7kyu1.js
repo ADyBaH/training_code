aa51311000fb140485166cc26f86cefd3dc1947d
ADyBaH <Brandon4@mail.ru>
2023-03-23 12:56:44 +0300

    feat: solution for kataStrings6kyu0.ts

--- codewars/kataOOP7kyu1.js
+++
@@ -1,6 +0,0 @@
-const myFunc = function() {
-  return myFunc
-}
-
-const newFunction = myFunc();
-console.log(new new new newFunction)
---
+++ codewars/ts/kataStrings6kyu0.ts
@@ -0,0 +1,53 @@
+/*
+You probably know the "like" system from Facebook and other pages.
+People can "like" blog posts, pictures or other items.
+We want to create the text that should be displayed next to such an item.
+
+Implement the function which takes an array containing the names of people that like an item. 
+It must return the display text as shown in the examples:
+
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+Note: For 4 or more names, the number in "and 2 others" simply increases.
+*/
+
+export const likes = (names: string[]): string => {
+  const answer: string[] = [
+    "no one likes this",
+    `${names[0]} likes this`,
+    `${names[0]} and ${names[1]} like this`,
+    `${names[0]}, ${names[1]} and ${names[2]} like this`,
+    `${names[0]}, ${names[1]} and ${names.length - 2} others like this`,
+  ];
+  return names.length <= 3 ? answer[names.length] : answer[4];
+};
+
+// console.log(likes([])); //, 'no one likes this');
+// console.log(likes(["Peter"])); //, 'Peter likes this');
+// console.log(likes(["Jacob", "Alex"])); //, 'Jacob and Alex like this');
+// console.log(likes(["Max", "John", "Mark"])); //, 'Max, John and Mark like this');
+// console.log(likes(["Alex", "Jacob", "Mark", "Max"])); //, 'Alex, Jacob and 2 others like this');
+
+// better solution:
+export const likes1 = (names: string[]): string => {
+  return {
+    0 : 'no one likes this',
+    1 : `${names[0]} likes this`,
+    2 : `${names[0]} and ${names[1]} like this`,
+    3 : `${names[0]}, ${names[1]} and ${names[2]} like this`,
+    4 : `${names[0]}, ${names[1]} and ${names.length - 2} others like this`,
+  }[Math.min(4, names.length)] as string
+  /* 
+  [
+      'no one likes this',
+      `${names[0]} likes this`,
+      `${names[0]} and ${names[1]} like this`,
+      `${names[0]}, ${names[1]} and ${names[2]} like this`,
+      `${names[0]}, ${names[1]} and ${names.length - 2} others like this`,
+  ][Math.min(4, names.length)];
+    */
+}
