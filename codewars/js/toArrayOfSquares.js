5a8286d0b26f85cc560fb1b16ffdd6cf372ca33b
ADyBaH <Brandon4@mail.ru>
2023-04-22 11:42:04 +0300

    feat: add solution for toArrayOfSquares.js

---
+++ codewars/js/toArrayOfSquares.js
@@ -0,0 +1,12 @@
+/**
+ * Transforms the numeric array into the according array of squares:
+ *   f(x) = x * x
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *   [ 0, 1, 2, 3, 4, 5 ] => [ 0, 1, 4, 9, 16, 25 ]
+ *   [ 10, 100, -1 ]      => [ 100, 10000, 1 ]
+ */
+const toArrayOfSquares = (arr) => arr.map((value) => value * value);
