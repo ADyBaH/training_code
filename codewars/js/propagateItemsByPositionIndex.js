d89b92c03cb21d3c021dc47a018a04522cae53ea
ADyBaH <Brandon4@mail.ru>
2023-04-28 10:38:43 +0300

    refactor: added better solution for propagateItemsByPositionIndex

--- codewars/js/propagateItemsByPositionIndex.js
+++ codewars/js/propagateItemsByPositionIndex.js
@@ -5,7 +5,11 @@
 *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
 *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
 */
-function propagateItemsByPositionIndex(arr) {
+
+const propagateItemsByPositionIndex = (arr) => arr.map((v, i) => new Array(i + 1).fill(v)).flat(Infinity);
+
+/*
+old solution:
   let answer = arr.slice(0);
   answer = answer.map((v, i) => {
     const mapArr = new Array(i + 1);
@@ -13,20 +17,18 @@ function propagateItemsByPositionIndex(arr) {
     return mapArr
   });
   return answer.flat(Infinity);
-  
-  // Previous solution:
-  // const answer = [];
-  // arr.map((value, index) => {
-  //   const mapArr = new Array(index + 1);
-  //   mapArr.fill(value);
-  //   answer.push(...mapArr);
-  //   return false;
-  // });
-  // return answer;
- }
- 
- console.log(propagateItemsByPositionIndex([]))// => []
- console.log(propagateItemsByPositionIndex([ 1 ]))// => [ 1 ]
- console.log(propagateItemsByPositionIndex([ 'a', 'b' ]))// => [ 'a', 'b','b' ]
- console.log(propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]))// => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
- console.log(propagateItemsByPositionIndex([ 1,2,3,4,5 ]))// => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
+  }
+
+old solution:
+  function propagateItemsByPositionIndex(arr) {
+    // throw new Error('Not implemented');
+    const answer = [];
+    arr.map((value, index) => {
+      const mapArr = new Array(index + 1);
+      mapArr.fill(value);
+      answer.push(...mapArr);
+      return false;
+    });
+    return answer;
+  }
+*/
