bb240c86d16c1940d69ed80ef3148c914e64724a
ADyBaH <Brandon4@mail.ru>
2023-04-30 11:42:52 +0300

    feat: add better solution for getIntervalArray

--- codewars/js/getIntervalArray.js
+++ codewars/js/getIntervalArray.js
@@ -1,16 +1,27 @@
-/*
+/**
  * Creates an array of integers from the specified start to end (inclusive)
-
+ *
+ * @param {number} start
+ * @param {number} end
+ * @return {array}
+ *
+ * @example
  *     1, 5  => [ 1, 2, 3, 4, 5 ]
  *    -2, 2  => [ -2, -1, 0, 1, 2 ]
  *     0, 100 => [ 0, 1, 2, ..., 100 ]
  *     3, 3   => [ 3 ]
  */
-function getIntervalArray(start, end) {
-  const arrayLength = end - start;
-  const arr = Array.from({ length: arrayLength + 1 }, (fromValue, fromIndex) => {
-    const arrayFrom = start + fromIndex;
-    return arrayFrom;
-  });
-  return arr;
-}
+
+const getIntervalArray = (start, end) => Array.from({length: end - start + 1}).map((_, i) => i + start);
+
+/*
+  old solution
+  function getIntervalArray(start, end) {
+    const arrayLength = end - start;
+    const arr = Array.from({ length: arrayLength + 1 }, (fromValue, fromIndex) => {
+      const arrayFrom = start + fromIndex;
+      return arrayFrom;
+    });
+    return arr;
+  }
+*/
