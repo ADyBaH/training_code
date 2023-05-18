add21b06a0a8be64b20a98eacae26fa35311e1a8
ADyBaH <Brandon4@mail.ru>
2023-05-18 11:44:51 +0300

    feat: add refactor solution for getStringLength

---
+++ algorithms/random/getStringLength.js
@@ -0,0 +1,21 @@
+/**
+ * Returns the length of given string.
+ *
+ * @param {string} value
+ * @return {number}
+ *
+ * @example
+ *   'aaaaa' => 5
+ *   'b'     => 1
+ *   ''      => 0
+ */
+
+const getStringLength = (value) => value.length;
+
+/*
+old solution:
+  function getStringLength(value) {
+    // throw new Error('Not implemented');
+    return value.length;
+  }
+*/
