40dee0c9d0e068bcc91c402e24421d2157fa9b68
ADyBaH <Brandon4@mail.ru>
2023-05-20 12:32:38 +0300

    feat: add solution for extractNameFromTemplate

---
+++ algorithms/random/extractNameFromTemplate.js
@@ -0,0 +1,11 @@
+/**
+ * Extracts a name from template string 'Hello, First_Name Last_Name!'.
+ *
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'Hello, John Doe!' => 'John Doe'
+ *   'Hello, Chuck Norris!' => 'Chuck Norris'
+ */
+const extractNameFromTemplate = (value) => value.split(',')[1].split('!')[0].trim();
