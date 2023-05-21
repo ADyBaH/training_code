e835a5e6660e28161aa4496df5ff4e8d23f3f229
ADyBaH <Brandon4@mail.ru>
2023-05-21 11:25:49 +0300

    feat: add solution for getFirstChar

---
+++ algorithms/random/getFirstChar.js
@@ -0,0 +1,11 @@
+/**
+ * Returns a first char of the given string.
+ *
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'John Doe'  => 'J'
+ *   'cat'       => 'c'
+ */
+const getFirstChar = (value) => value[0]
