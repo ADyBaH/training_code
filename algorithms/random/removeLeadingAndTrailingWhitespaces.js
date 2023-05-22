d31fe8347f98b3b8692155bdadc05c628c9b10cf
ADyBaH <Brandon4@mail.ru>
2023-05-22 11:17:47 +0300

    feat: add solution for removeLeadingAndTrailingWhitespaces

---
+++ algorithms/random/removeLeadingAndTrailingWhitespaces.js
@@ -0,0 +1,12 @@
+/**
+ * Removes a leading and trailing whitespace characters from string.
+ *
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   '  Abracadabra'    => 'Abracadabra'
+ *   'cat'              => 'cat'
+ *   '\tHello, World! ' => 'Hello, World!'
+ */
+const removeLeadingAndTrailingWhitespaces = (value) => value.trim();
