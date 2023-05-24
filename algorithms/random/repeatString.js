4ac20e250192cc1e95a98f76eb5026fe7ce38cdd
ADyBaH <Brandon4@mail.ru>
2023-05-24 12:31:48 +0300

    feat: add solution for repeatString

---
+++ algorithms/random/repeatString.js
@@ -0,0 +1,12 @@
+/**
+ * Returns a string that repeated the specified number of times.
+ *
+ * @param {string} value
+ * @param {string} count
+ * @return {string}
+ *
+ * @example
+ *   'A', 5  => 'AAAAA'
+ *   'cat', 3 => 'catcatcat'
+ */
+const repeatString = (value, count) => value.repeat(count)
