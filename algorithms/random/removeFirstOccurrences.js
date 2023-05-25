831f69059c28b73c4f8ac7ef5529a1613a92d593
ADyBaH <Brandon4@mail.ru>
2023-05-25 13:02:18 +0300

    feat: add solution for removeFirstOccurrences

---
+++ algorithms/random/removeFirstOccurrences.js
@@ -0,0 +1,14 @@
+/**
+ * Remove the first occurrence of string inside another string
+ *
+ * @param {string} str
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'To be or not to be', 'not'  => 'To be or  to be'
+ *   'I like legends', 'end' => 'I like legs',
+ *   'ABABAB','BA' => 'ABAB'
+ */
+
+const removeFirstOccurrences = (str, value) => str.replace(value, '')
