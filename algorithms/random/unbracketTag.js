054c03a7efcb9c0b918d0c440c804aa29b088fff
ADyBaH <Brandon4@mail.ru>
2023-05-26 12:56:54 +0300

    feat: add solution for unbracketTag

---
+++ algorithms/random/unbracketTag.js
@@ -0,0 +1,13 @@
+/**
+ * Remove the first and last angle brackets from tag string
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example
+ *   '<div>' => 'div'
+ *   '<span>' => 'span'
+ *   '<a>' => 'a'
+ */
+
+const unbracketTag = (str) => str.replace('<', '').replace('>', '');
