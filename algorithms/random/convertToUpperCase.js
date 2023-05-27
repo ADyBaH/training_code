921441e9b3559dcd7e39d4f4847cc3272702f82c
ADyBaH <Brandon4@mail.ru>
2023-05-27 12:28:02 +0300

    feat: add solution for convertToUpperCase

---
+++ algorithms/random/convertToUpperCase.js
@@ -0,0 +1,11 @@
+/**
+ * Converts all characters of the specified string into the upper case
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example
+ *   'Thunderstruck' => 'THUNDERSTRUCK'
+ *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+ */
+const convertToUpperCase = (str) => str.toUpperCase();
