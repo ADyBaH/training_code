9dd226e329a284ffc6c74919dbde0ca9ea32f90b
ADyBaH <Brandon4@mail.ru>
2023-05-29 13:18:39 +0300

    feat: add solution for isString

---
+++ algorithms/random/isString.js
@@ -0,0 +1,14 @@
+/**
+ * Returns true if the value is string; otherwise false.
+ * @param {string} value
+ * @return {boolean}
+ *
+ * @example
+ *   isString() => false
+ *   isString(null) => false
+ *   isString([]) => false
+ *   isString({}) => false
+ *   isString('test') => true
+ *   isString(new String('test')) => true
+ */
+const isString =(value) => (typeof value === 'string' || value instanceof String);
