e7cb34db843f0bf041750283223b5284ed4b0fca
ADyBaH <Brandon4@mail.ru>
2023-06-01 12:25:11 +0300

    feat: add solution for getRectangleArea

---
+++ algorithms/random/getRectangleArea.js
@@ -0,0 +1,12 @@
+/**
+ * Returns an area of a rectangle given by width and height.
+ *
+ * @param {number} width
+ * @param {number} height
+ * @return {number}
+ *
+ * @example:
+ *   5, 10 => 50
+ *   5, 5  => 25
+ */
+const getRectangleArea = (width, height) => width * height;
