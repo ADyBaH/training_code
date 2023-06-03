ff35940e4bd5e51c258bd437090233c695993f6f
ADyBaH <Brandon4@mail.ru>
2023-06-03 13:10:49 +0300

    feat: add solution for getAverage

---
+++ algorithms/random/getAverage.js
@@ -0,0 +1,15 @@
+/**
+ * Returns an average of two given numbers.
+ *
+ * @param {number} value1
+ * @param {number} value2
+ * @return {number}
+ *
+ * @example:
+ *   5, 5  => 5
+ *  10, 0  => 5
+ *  -3, 3  => 0
+ */
+function getAverage(value1, value2) {
+  return (BigInt(value1) + BigInt(value2)) / BigInt(2);
+}
