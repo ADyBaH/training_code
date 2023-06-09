b8cd49d3c14c33f2e44e23f38cbb34102df7da0e
ADyBaH <Brandon4@mail.ru>
2023-06-09 13:29:10 +0300

    feat: add solution for getLastDigit

---
+++ algorithms/random/getLastDigit.js
@@ -0,0 +1,14 @@
+/**
+ * Returns a last digit of a integer number.
+ *
+ * @param {number} value
+ * @return {number}
+ *
+ * @example:
+ *   100     => 0
+ *    37     => 7
+ *     5     => 5
+ *     0     => 0
+ */
+
+const getLastDigit = (value) => value % 10;
