4173d13ff8d85f7b4340be6a247b7992333a282e
ADyBaH <Brandon4@mail.ru>
2023-06-10 14:11:01 +0300

    feat: add solution for parseNumberFromString

---
+++ algorithms/random/parseNumberFromString.js
@@ -0,0 +1,12 @@
+/**
+ * Returns a number by given string representation.
+ *
+ * @param {string} value
+ * @return {number}
+ *
+ * @example:
+ *    '100'     => 100
+ *     '37'     => 37
+ * '-525.5'     => -525.5
+ */
+const parseNumberFromString = (value) =>  +value;
