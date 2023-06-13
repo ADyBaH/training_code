9768e9896de512a9b2bdca975f87e86cc497d1f0
ADyBaH <Brandon4@mail.ru>
2023-06-13 14:22:15 +0300

    feat: add new solution for isPrime

--- algorithms/random/isPrime.js
+++ algorithms/random/isPrime.js
@@ -15,12 +15,16 @@
  *   16 => false
  *   17 => true
  */
-function isPrime(n) {
-  return new Array(n)
-    .fill(0)
-    .map((_, i) => i)
-    .reduce((acc = 0, _, i, arr) => ((arr.length + 1) % i === 0) ? acc += 1 : acc) > 1;
-}
+
+const isPrime = (n) =>
+  [n].reduce((acc, number) => {
+    for (let i = 2; i < number; i += 1) {
+      if (n % i === 0) acc.push(i);
+    }
+    return acc;
+  }, []) <= 0;
+
+
 /*
   old solution:
   const answer = [];
@@ -28,4 +32,12 @@ function isPrime(n) {
     if (n % i === 0) answer.push(i);
   }
   return answer.length <= 0;
+*/
+/*
+  function isPrime(n) {
+  return new Array(n)
+    .fill(0)
+    .map((_, i) => i)
+    .reduce((acc = 0, _, i, arr) => ((arr.length + 1) % i === 0) ? acc += 1 : acc) > 1;
+}
 */
