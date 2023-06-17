6ed71f0bc85c652a26c3247d6c46703b4d2080cd
ADyBaH <Brandon4@mail.ru>
2023-06-17 14:44:45 +0300

    feat: add solution for doubleArray

---
+++ algorithms/random/doubleArray.js
@@ -0,0 +1,14 @@
+/**
+ * Returns the doubled array - elements of the specified array
+ * are repeated twice using original order
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    ['Ace', 10, true]  => ['Ace', 10, true,   'Ace', 10, true]
+ *    [0, 1, 2, 3, 4, 5] => [0, 1, 2, 3, 4, 5,   0, 1, 2, 3, 4, 5]
+ *    [] => []
+ */
+
+const doubleArray = (arr) => arr.concat(arr);
