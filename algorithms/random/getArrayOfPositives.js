3eeac800e75c22193437599a73338c11c4d3c845
ADyBaH <Brandon4@mail.ru>
2023-06-18 14:01:10 +0300

    feat: add solution for getArrayOfPositives

---
+++ algorithms/random/getArrayOfPositives.js
@@ -0,0 +1,12 @@
+/**
+ * Returns an array of positive numbers from the specified array in original order
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    [ 0, 1, 2, 3, 4, 5 ] => [ 1, 2, 3, 4, 5 ]
+ *    [-1, 2, -5, -4, 0] => [ 2 ]
+ *    [] => []
+ */
+const getArrayOfPositives = (arr) => arr.filter((a) => a > 0);
