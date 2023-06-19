4734405db5fd16b4ffb48a2eefad4f5414f048d6
ADyBaH <Brandon4@mail.ru>
2023-06-19 15:14:57 +0300

    feat: add solution for getArrayOfStrings

---
+++ algorithms/random/getArrayOfStrings.js
@@ -0,0 +1,13 @@
+/**
+ * Returns the array with strings only in the specified array (in original order)
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    [ 0, 1, 'cat', 3, true, 'dog' ] => [ 'cat', 'dog' ]
+ *    [ 1, 2, 3, 4, 5 ] => []
+ *    [ 'cat, 'dog', 'raccoon' ] => [ 'cat', 'dog', 'raccoon' ]
+ */
+
+const getArrayOfStrings = (arr) => arr.filter((a) => typeof a === 'string');
