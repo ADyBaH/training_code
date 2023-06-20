d2aed232417123f24d76dde70487b93b9f5faf1e
ADyBaH <Brandon4@mail.ru>
2023-06-20 09:42:08 +0300

    feat: add solution for getUpperCaseStrings

---
+++ algorithms/random/getUpperCaseStrings.js
@@ -0,0 +1,13 @@
+/**
+ * Returns the array of uppercase strings from the specified array
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    [ 'permanent-internship', 'glutinous-shriek', 'multiplicative-elevation' ]
+ *    => [ 'PERMANENT-INTERNSHIP', 'GLUTINOUS-SHRIEK', 'MULTIPLICATIVE-ELEVATION' ],
+ *    [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]  => [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ]
+ */
+
+const getUpperCaseStrings = (arr) => arr.map((a) => a.toUpperCase());
