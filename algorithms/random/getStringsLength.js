f5663115c30c9dc0bd1c2e0410df23c67fa41601
ADyBaH <Brandon4@mail.ru>
2023-06-21 12:12:57 +0300

    feat: add solution for getStringsLength

---
+++ algorithms/random/getStringsLength.js
@@ -0,0 +1,12 @@
+/**
+ * Returns the array of string lengths from the specified string array.
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *    [ '', 'a', 'bc', 'def', 'ghij' ]  => [ 0, 1, 2, 3, 4 ]
+ *    [ 'angular', 'react', 'ember' ] => [ 7, 5, 5 ]
+ */
+
+const getStringsLength = (arr) => arr.map((a) => a.length);
