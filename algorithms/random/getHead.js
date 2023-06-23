7befd8203345c451b9498a701cb8dfd993e3f045
ADyBaH <Brandon4@mail.ru>
2023-06-23 14:41:58 +0300

    feat: add solution for getHead

---
+++ algorithms/random/getHead.js
@@ -0,0 +1,12 @@
+/**
+ * Returns the n first items of the specified array
+ *
+ * @param {array} arr
+ * @param {number} n
+ *
+ * @example
+ *    [ 1, 3, 4, 5 ], 2 => [ 1, 3 ]
+ *    [ 'a', 'b', 'c', 'd'], 3  => [ 'a', 'b', 'c' ]
+ */
+
+const getHead = (arr, n) => (arr.splice(n), arr);
