3540b4c14195ae0d479d419e7058f7f079b59293
ADyBaH <Brandon4@mail.ru>
2023-06-24 15:01:50 +0300

    feat: add solution for getTail

---
+++ algorithms/random/getTail.js
@@ -0,0 +1,12 @@
+/**
+ * Returns the n last items of the specified array
+ *
+ * @param {array} arr
+ * @param {number} n
+ *
+ * @example
+ *    [ 1, 3, 4, 5 ], 2  => [ 4, 5 ]
+ *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
+ */
+
+const getTail = (arr, n) => arr.slice(-n);
