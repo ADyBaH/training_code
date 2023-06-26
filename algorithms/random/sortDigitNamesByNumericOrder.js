1ee3701ee3155024647c3adad3479d15418f091c
ADyBaH <Brandon4@mail.ru>
2023-06-26 13:24:05 +0300

    feat: add solution for sortDigitNamesByNumericOrder

---
+++ algorithms/random/sortDigitNamesByNumericOrder.js
@@ -0,0 +1,30 @@
+/**
+ * Sorts digit names
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *   [] => []
+ *   [ 'nine','one' ]                 => [ 'one', 'nine' ]
+ *   [ 'one','two','three' ]          => [ 'one','two', 'three' ]
+ *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
+ *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
+ */
+const sortDigitNamesByNumericOrder = (arr, sortBy) =>
+  arr.sort((a, b) => sortBy.indexOf(a) - sortBy.indexOf(b));
+
+const arrNumber = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+
+console.log(sortDigitNamesByNumericOrder([ 'nine','one' ], arrNumber))
