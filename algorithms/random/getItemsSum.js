e1eef508548853c527c6dccce7cde54e9c6fb879
ADyBaH <Brandon4@mail.ru>
2023-06-27 14:12:56 +0300

    feat: add solution for getItemsSum

---
+++ algorithms/random/getItemsSum.js
@@ -0,0 +1,14 @@
+/**
+ * Returns the sum of all items in the specified array of numbers
+ *
+ * @param {array} arr
+ * @return {number}
+ *
+ * @example
+ *   [] => 0
+ *   [ 1, 2, 3 ]           => 6
+ *   [ -1, 1, -1, 1 ]      => 0
+ *   [ 1, 10, 100, 1000 ]  => 1111
+ */
+
+const getItemsSum = (arr) => arr.reduce((acc, value) => acc + value, 0);
