d6df527e3ce9beaacb31b860b88eec50fc3f0c8b
ADyBaH <Brandon4@mail.ru>
2023-06-28 12:42:50 +0300

    feat: add solution for toStringList

---
+++ algorithms/random/toStringList.js
@@ -0,0 +1,13 @@
+/**
+ * Concatenates all elements from specified array into single string with ',' delimiter
+ *
+ * @param {array} arr
+ * @return {string}
+ *
+ * @example
+ *    [0, false, 'cat', NaN, true, '']  => '0,false,cat,NaN,true,'
+ *    [1, 2, 3, 4, 5]                   => '1,2,3,4,5'
+ *    ['rock', 'paper', 'scissors']     => 'rock,paper,scissors'
+ */
+
+const toStringList = (arr) => arr.join();
