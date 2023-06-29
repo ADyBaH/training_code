ead5f982c8540b5c4f1e8cd8d6bf66a4b637b427
ADyBaH <Brandon4@mail.ru>
2023-06-29 13:33:53 +0300

    feat: add solution for distinct

---
+++ algorithms/random/distinct.js
@@ -0,0 +1,13 @@
+/**
+ * Returns array containing only unique values from the specified array.
+ *
+ * @param {array} arr
+ * @return {array}
+ *
+ * @example
+ *   [ 1, 2, 3, 3, 2, 1 ] => [ 1, 2, 3 ]
+ *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
+ *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
+ */
+
+const distinct = (arr) => [...new Set(arr)];
