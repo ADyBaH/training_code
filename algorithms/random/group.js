3026689cd2b2730551782954f987fd86f49688e9
ADyBaH <Brandon4@mail.ru>
2023-07-02 13:24:13 +0300

    feat: add solution for group

---
+++ algorithms/random/group.js
@@ -0,0 +1,44 @@
+/**
+ * Groups elements of the specified array by key.
+ * Returns multimap of keys extracted from array elements via keySelector callback
+ * and values extracted via valueSelector callback.
+ * See: https://en.wikipedia.org/wiki/Multimap
+ *
+ * @param {array} array
+ * @param {Function} keySelector
+ * @param {Function} valueSelector
+ * @return {Map}
+ *
+ * @example
+ *   group([
+ *      { country: 'Belarus', city: 'Brest' },
+ *      { country: 'Russia', city: 'Omsk' },
+ *      { country: 'Russia', city: 'Samara' },
+ *      { country: 'Belarus', city: 'Grodno' },
+ *      { country: 'Belarus', city: 'Minsk' },
+ *      { country: 'Poland', city: 'Lodz' }
+ *     ],
+ *     item => item.country,
+ *     item => item.city
+ *   )
+ *            =>
+ *   Map {
+ *    "Belarus" => ["Brest", "Grodno", "Minsk"],
+ *    "Russia" => ["Omsk", "Samara"],
+ *    "Poland" => ["Lodz"]
+ *   }
+ */
+function group(array, keySelector, valueSelector) {
+  const map = new Map();
+
+  array.map((value) => {
+    if (!map.has(keySelector(value))) {
+      map.set(keySelector(value), [valueSelector(value)]);
+    } else {
+      const arr = [...map.get(keySelector(value)), valueSelector(value)];
+      map.set(keySelector(value), arr);
+    }
+    return true;
+  });
+  return map;
+}
