a771d6fd08ef6545fa001c9134b93527d1d9588b
ADyBaH <Brandon4@mail.ru>
2023-07-03 12:58:12 +0300

    feat: add solution for selectMany

---
+++ algorithms/random/selectMany.js
@@ -0,0 +1,17 @@
+
+/**
+ * Projects each element of the specified array to a sequence
+ * and flattens the resulting sequences into one array.
+ *
+ * @param {array} arr
+ * @param {Function} childrenSelector, a transform function to apply to each element
+ *                                     that returns an array of children
+ * @return {array}
+ *
+ * @example
+ *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
+ *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
+ */
+
+const selectMany = (arr, childrenSelector) => arr.flatMap((value) => childrenSelector(value))
+
