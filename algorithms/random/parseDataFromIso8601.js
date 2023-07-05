306e1dc23a62a1dfdbf0fc05577f8544c5a55beb
ADyBaH <Brandon4@mail.ru>
2023-07-05 13:16:40 +0300

    feat: add solution for parseDataFromIso8601

---
+++ algorithms/random/parseDataFromIso8601.js
@@ -0,0 +1,13 @@
+/**
+ * Parses an ISO 8601 string date representation into date value
+ * For ISO 8601 date specification refer to : https://en.wikipedia.org/wiki/ISO_8601
+ *
+ * @param {string} value
+ * @return {date}
+ *
+ * @example :
+ *    '2016-01-19T16:07:37+00:00'    => Date()
+ *    '2016-01-19T08:07:37Z' => Date()
+ */
+
+const parseDataFromIso8601 = (value) => new Date(Date.parse(value));
