75e86819d0b60a6ee3aa34cca5cee1af50e40a58
ADyBaH <Brandon4@mail.ru>
2023-07-06 13:36:40 +0300

    feat: add solution for isLeapYear

---
+++ algorithms/random/isLeapYear.js
@@ -0,0 +1,16 @@
+/**
+ * Returns true if specified date is leap year and false otherwise
+ * Please find algorithm here: https://en.wikipedia.org/wiki/Leap_year#Algorithm
+ *
+ * @param {date} date
+ * @return {bool}
+ *
+ * @example :
+ *    Date(1900,1,1)    => false
+ *    Date(2000,1,1)    => true
+ *    Date(2001,1,1)    => false
+ *    Date(2012,1,1)    => true
+ *    Date(2015,1,1)    => false
+ */
+
+const isLeapYear = (date) => new Date(date.setDate(29)).getMonth() === 1;
