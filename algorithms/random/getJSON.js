63b301e0e7aedc67583a79db0c47d5b6153080cb
ADyBaH <Brandon4@mail.ru>
2023-07-08 14:08:03 +0300

    feat: add solution for getJSON

---
+++ algorithms/random/getJSON.js
@@ -0,0 +1,12 @@
+/**
+ * Returns the JSON representation of specified object
+ *
+ * @param {object} obj
+ * @return {string}
+ *
+ * @example
+ *    [1,2,3]   =>  '[1,2,3]'
+ *    { width: 10, height : 20 } => '{"height":10,"width":20}'
+ */
+
+const getJSON = (obj) => JSON.stringify(obj);
