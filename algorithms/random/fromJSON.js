e56ba72fedd78fee56da5f6fbd87cf35f53012a8
ADyBaH <Brandon4@mail.ru>
2023-07-09 11:50:07 +0300

    feat: add solution for fromJSON

---
+++ algorithms/random/fromJSON.js
@@ -0,0 +1,16 @@
+/**
+ * Returns the object of specified type from JSON representation
+ *
+ * @param {Object} proto
+ * @param {string} json
+ * @return {object}
+ *
+ * @example
+ *    const r = fromJSON(Circle.prototype, '{"radius":10}');
+ *
+ */
+function fromJSON(proto, json) {
+  const getobject = JSON.parse(json);
+  Object.setPrototypeOf(getobject, proto);
+  return getobject;
+}
