5a42352fdd78573448fcb3a69eb5f95c3a654b37
ADyBaH <Brandon4@mail.ru>
2023-07-12 10:10:31 +0300

    feat: add solution for getFastestPromise

---
+++ algorithms/random/getFastestPromise.js
@@ -0,0 +1,21 @@
+/**
+ * Return Promise object that should be resolved with value received from
+ * Promise object that will be resolved first.
+ * Function receive an array of Promise objects.
+ *
+ * @param {Promise[]} array
+ * @return {Promise}
+ *
+ * @example
+ *    const promises = [
+ *      Promise.resolve('first'),
+ *      new Promise(resolve => setTimeout(() => resolve('second'), 500)),
+ *    ];
+ *    const p = processAllPromises(promises);
+ *    p.then((res) => {
+ *      console.log(res) // => [first]
+ *    })
+ *
+ */
+
+const getFastestPromise = (array) => Promise.race(array);
