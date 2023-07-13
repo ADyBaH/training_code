625451fd7d9a48e0edddf75d4ef76c8559b3e978
ADyBaH <Brandon4@mail.ru>
2023-07-13 12:23:20 +0300

    feat: add solution for getFizzBuzz

---
+++ algorithms/random/getFizzBuzz.js
@@ -0,0 +1,32 @@
+/**
+ * Returns the 'Fizz','Buzz' or an original number using the following rules:
+ * 1) return original number
+ * 2) but if number multiples of three return 'Fizz'
+ * 3) for the multiples of five return 'Buzz'
+ * 4) for numbers which are multiples of both three and five return 'FizzBuzz'
+ *
+ * @param {number} num
+ * @return {any}
+ *
+ * @example
+ *   2 =>  2
+ *   3 => 'Fizz'
+ *   5 => 'Buzz'
+ *   4 => 4
+ *  15 => 'FizzBuzz'
+ *  20 => 'Buzz'
+ *  21 => 'Fizz'
+ *
+ */
+function getFizzBuzz(num) {
+  if (num % 5 === 0 && num % 3 === 0) {
+    return 'FizzBuzz';
+  }
+  if (num % 5 === 0) {
+    return 'Buzz';
+  }
+  if (num % 3 === 0) {
+    return 'Fizz';
+  }
+  return num;
+}
