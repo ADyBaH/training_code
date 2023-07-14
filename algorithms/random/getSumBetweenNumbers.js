7d82c7aef39f2d2b872c85dfd47c70c3bc0256d3
ADyBaH <Brandon4@mail.ru>
2023-07-14 14:24:21 +0300

    feat: add solution for getSumBetweenNumbers

---
+++ algorithms/random/getSumBetweenNumbers.js
@@ -0,0 +1,19 @@
+/**
+ * Returns the sum of integer numbers between n1 and n2 (inclusive).
+ *
+ * @param {number} n1
+ * @param {number} n2
+ * @return {number}
+ *
+ * @example:
+ *   1,2   =>  3  ( = 1+2 )
+ *   5,10  =>  45 ( = 5+6+7+8+9+10 )
+ *   -1,1  =>  0  ( = -1 + 0 + 1 )
+ */
+function getSumBetweenNumbers(n1, n2) {
+  let answer = 0;
+  for (let i = n1; i <= n2; i += 1) {
+    answer += i;
+  }
+  return answer;
+}
