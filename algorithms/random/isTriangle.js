8c3f28fab5efda6b5fee8bc2b70c690f57adfb53
ADyBaH <Brandon4@mail.ru>
2023-07-15 11:31:02 +0300

    feat: add solution for isTriangle

---
+++ algorithms/random/isTriangle.js
@@ -0,0 +1,19 @@
+/**
+ * Returns true, if a triangle can be built with the specified sides a, b, c
+ * and false in any other ways.
+ *
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {bool}
+ *
+ * @example:
+ *   1,2,3    =>  false
+ *   3,4,5    =>  true
+ *   10,1,1   =>  false
+ *   10,10,10 =>  true
+ */
+function isTriangle(a, b, c) {
+  const arr = [a, b, c].sort((a1, b2) => a1 - b2);
+  return (arr[0] + arr[1]) > arr[2];
+}
