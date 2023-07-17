fde52502d9d0dd06a8d0f8f49e5b732c701ac3b3
ADyBaH <Brandon4@mail.ru>
2023-07-17 13:51:08 +0300

    feat: add solution for isInsideCircle

---
+++ algorithms/random/isInsideCircle.js
@@ -0,0 +1,32 @@
+/**
+ * Returns true, if point lies inside the circle, otherwise false.
+ * Circle is an object of
+ *  {
+ *     center: {
+ *       x: 5,
+ *       y: 5
+ *     },
+ *     radius: 20
+ *  }
+ *
+ * Point is object of
+ *  {
+ *     x: 5,
+ *     y: 5
+ *  }
+ *
+ * @param {object} circle
+ * @param {object} point
+ * @return {bool}
+ *
+ * @example:
+ *   { center: { x:0, y:0 }, radius:10 },  { x:0, y:0 }     => true
+ *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
+ *
+ */
+function isInsideCircle({center, radius}, {x, y}) {
+  const centerX = Math.abs(x - center.x);
+  const centerY = Math.abs(y - center.y);
+
+  return (centerX + centerY) < radius;
+}
