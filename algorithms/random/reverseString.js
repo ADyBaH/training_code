d6e894223b85d8669d99aab7b55f7028ffff5bd1
ADyBaH <Brandon4@mail.ru>
2023-07-19 13:04:44 +0300

    feat: add solution for reverseString

---
+++ algorithms/random/reverseString.js
@@ -0,0 +1,14 @@
+/**
+ * Reverse the specified string (put all chars in reverse order)
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example:
+ * 'The quick brown fox jumps over the lazy dog' => 'god yzal eht revo spmuj xof nworb kciuq ehT'
+ * 'abracadabra' => 'arbadacarba'
+ * 'rotator' => 'rotator'
+ * 'noon' => 'noon'
+ */
+
+const reverseString = (str) => str.split('').reverse().join('');
