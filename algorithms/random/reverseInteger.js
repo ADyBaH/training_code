76bc97857cdb21e4367a4410825220d10d6d6ee6
ADyBaH <Brandon4@mail.ru>
2023-07-20 13:27:35 +0300

    feat: add solution for reverseInteger

---
+++ algorithms/random/reverseInteger.js
@@ -0,0 +1,14 @@
+/**
+ * Reverse the specified integer number (put all digits in reverse order)
+ *
+ * @param {number} num
+ * @return {number}
+ *
+ * @example:
+ *   12345 => 54321
+ *   1111  => 1111
+ *   87354 => 45378
+ *   34143 => 34143
+ */
+
+const reverseInteger = (num) => +`${num}`.split('').reverse().join('')
