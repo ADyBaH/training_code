e6f0527307b90d07abd3908b4df8c254610e3138
ADyBaH <Brandon4@mail.ru>
2023-07-22 13:26:36 +0300

    feat: add solution for getDigitalRoot

---
+++ algorithms/random/getDigitalRoot.js
@@ -0,0 +1,19 @@
+/**
+ * Returns the digital root of integer:
+ *   step1 : find sum of all digits
+ *   step2 : if sum > 9 then goto step1 otherwise return the sum
+ *
+ * @param {number} n
+ * @return {number}
+ *
+ * @example:
+ *   12345 ( 1+2+3+4+5 = 15, 1+5 = 6) => 6
+ *   23456 ( 2+3+4+5+6 = 20, 2+0 = 2) => 2
+ *   10000 ( 1+0+0+0+0 = 1 ) => 1
+ *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
+ */
+function getDigitalRoot(num) {
+  const answer = `${num}`.split('').reduce((acc, v) => acc + +v, 0);
+  if (answer < 10) return answer;
+  return `${answer}`.split('').reduce((acc, v) => acc + +v, 0);
+}
