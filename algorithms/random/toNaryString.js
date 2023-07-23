c7d2f13d36d4c394b7df6038b6f7b67fb26f4fe3
ADyBaH <Brandon4@mail.ru>
2023-07-23 12:37:20 +0300

    feat: add solution for toNaryString

---
+++ algorithms/random/toNaryString.js
@@ -0,0 +1,22 @@
+/**
+ * Returns the string with n-ary (binary, ternary, etc, where n <= 10)
+ * representation of specified number.
+ * See more about
+ * https://en.wikipedia.org/wiki/Binary_number
+ * https://en.wikipedia.org/wiki/Ternary_numeral_system
+ * https://en.wikipedia.org/wiki/Radix
+ *
+ * @param {number} num
+ * @param {number} n, radix of the result
+ * @return {string}
+ *
+ * @example:
+ *   1024, 2  => '10000000000'
+ *   6561, 3  => '100000000'
+ *    365, 2  => '101101101'
+ *    365, 3  => '111112'
+ *    365, 4  => '11231'
+ *    365, 10 => '365'
+ */
+
+const toNaryString = (num, n) => num.toString(n)
