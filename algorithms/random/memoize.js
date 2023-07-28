d32d951bd1c698deeb56db73eaaa5e497b6a55b1
ADyBaH <Brandon4@mail.ru>
2023-07-28 13:10:43 +0300

    feat: add solution for memoize

---
+++ algorithms/random/memoize.js
@@ -0,0 +1,23 @@
+/**
+ * Memoizes passed function and returns function
+ * which invoked first time calls the passed function and then always returns cached result.
+ *
+ * @params {Function} func - function to memoize
+ * @return {Function} memoized function
+ *
+ * @example
+ *   const memoizer = memoize(() => Math.random());
+ *   memoizer() => some random number  (first run, evaluates the result of Math.random())
+ *   memoizer() => the same random number  (second run, returns the previous cached result)
+ *   ...
+ *   memoizer() => the same random number  (next run, returns the previous cached result)
+ */
+function memoize(func) {
+  const box = new Map();
+  box.set(func, func());
+  return function f() {
+    if (box.has(func)) return box.get(func);
+    box.set(func, func());
+    return box.get(func);
+  };
+}
