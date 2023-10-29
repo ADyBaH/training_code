3c5170d44d3939a9c7d4366dc580b04a46df90d1
ADyBaH <Brandon4@mail.ru>
2023-10-29 13:33:52 +0300

    feat: add solution for kataStrings8kyu9

---
+++ algorithms/codewars/js/string/kataStrings8kyu9.js
@@ -0,0 +1,5 @@
+/*
+You need to write a function detect that will check if a string starts with Can someone explain,
+case sensitive. Return true if so, false otherwise.
+*/
+const detect = (comment) => comment.startsWith("Can someone explain");
