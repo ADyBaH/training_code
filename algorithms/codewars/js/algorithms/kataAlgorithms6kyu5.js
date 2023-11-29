323c1654dabb2507d35f1ea10784be8d2d068cd8
ADyBaH <Brandon4@mail.ru>
2023-11-29 14:23:14 +0300

    feat: add solution for kataAlgorithms6kyu5

---
+++ algorithms/codewars/js/algorithms/kataAlgorithms6kyu5.js
@@ -0,0 +1,80 @@
+/* 
+Your task is to implement a function that would take the morse 
+code as input and return a decoded human-readable string.
+
+For example:
+
+decodeMorse('.... . -.--   .--- ..- -.. .')
+//should return "HEY JUDE"
+*/
+
+const MORSE_CODE = {
+  "-.-.--": "!",
+  ".-..-.": '"',
+  "...-..-": "$",
+  ".-...": "&",
+  ".----.": "'",
+  "-.--.": "(",
+  "-.--.-": ")",
+  ".-.-.": "+",
+  "--..--": ",",
+  "-....-": "-",
+  ".-.-.-": ".",
+  "-..-.": "/",
+  "-----": "0",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "---...": ":",
+  "-.-.-.": ";",
+  "-...-": "=",
+  "..--..": "?",
+  ".--.-.": "@",
+  ".-": "A",
+  "-...": "B",
+  "-.-.": "C",
+  "-..": "D",
+  ".": "E",
+  "..-.": "F",
+  "--.": "G",
+  "....": "H",
+  "..": "I",
+  ".---": "J",
+  "-.-": "K",
+  ".-..": "L",
+  "--": "M",
+  "-.": "N",
+  "---": "O",
+  ".--.": "P",
+  "--.-": "Q",
+  ".-.": "R",
+  "...": "S",
+  "-": "T",
+  "..-": "U",
+  "...-": "V",
+  ".--": "W",
+  "-..-": "X",
+  "-.--": "Y",
+  "--..": "Z",
+  "..--.-": "_",
+  "...---...": "SOS",
+};
+
+/**
+ * Decode the given Morse code into a readable string.
+ *
+ * @param {string} morseCode - The Morse code to be decoded.
+ * @return {string} The decoded string.
+ */
+const decodeMorse = (morseCode) =>
+  morseCode
+    .split(" ")
+    .reduce((acc, value) => (value ? acc + MORSE_CODE[value] : acc + " "), "")
+    .replace(/\s+/g, " ")
+    .trim();
