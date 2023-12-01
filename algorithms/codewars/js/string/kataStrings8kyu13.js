73fca0d59ac7e5460cca2c0f798cae54de660599
ADyBaH <Brandon4@mail.ru>
2023-12-01 12:26:52 +0300

    feat: add solution for kataStrings8kyu13

---
+++ algorithms/codewars/js/string/kataStrings8kyu13.js
@@ -0,0 +1,11 @@
+/* 
+Write a function which converts the input string to uppercase.
+*/
+
+/**
+ * Convert the input string to uppercase.
+ *
+ * @param {string} str - The input string to be converted.
+ * @returns {string} The uppercase version of the input string.
+ */
+const makeUpperCase = (str) => str.toUpperCase();
