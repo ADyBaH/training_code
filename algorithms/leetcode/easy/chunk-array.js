89e05ae63f7b2ef40a52c2fa34929448030af2aa
Andrey N <brandon4@mail.ru>
2024-01-03 11:54:42 +0300

    feat: add solution for chunk-array.js

---
+++ algorithms/leetcode/easy/chunk-array.js
@@ -0,0 +1,25 @@
+/* 
+Given an array arr and a chunk size size, return a chunked array. 
+A chunked array contains the original elements in arr, but consists of subarrays 
+each of length size. The length of the last subarray may be less than size 
+if arr.length is not evenly divisible by size.
+
+You may assume the array is the output of JSON.parse. In other words, it is valid JSON.
+
+Please solve it without using lodash's _.chunk function.
+*/
+
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function (arr, size) {
+  const newArray = [];
+
+  for (let i = 0; i < arr.length; i += size) {
+    newArray.push(arr.slice(i, i + size));
+  }
+
+  return newArray;
+};
