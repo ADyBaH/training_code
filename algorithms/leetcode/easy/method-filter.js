74a85731f759dfa3ff2532f41f270caaa48af5a1
Andrey N <brandon4@mail.ru>
2024-01-04 14:52:46 +0300

    feat: add solution for method-filter.js

---
+++ algorithms/leetcode/easy/method-filter.js
@@ -0,0 +1,33 @@
+/* 
+Given an integer array arr and a filtering function fn, return a filtered array filteredArr.
+
+The fn function takes one or two arguments:
+
+arr[i] - number from the arr
+i - index of arr[i]
+filteredArr should only contain the elements from the arr for which the expression fn(arr[i], i) 
+evaluates to a truthy value. A truthy value is a value where Boolean(value) returns true.
+
+Please solve it without the built-in Array.filter method.
+ */
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function (arr, fn) {
+  if (arr.length === 0) {
+    return arr;
+  }
+
+  const newArray = [];
+
+  for (let i = arr.length - 1; i >= 0; i -= 1) {
+    if (Boolean(fn(arr[i], i))) {
+      newArray.unshift(arr[i]);
+    }
+  }
+
+  return newArray;
+};
