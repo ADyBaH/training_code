1dff9cb5506f72a2693e69dfadc5d7e9eb4455f4
Andrey N <brandon4@mail.ru>
2024-02-13 11:07:24 +0300

    feat: add solution for minimum-sum-of-four-digit.js

---
+++ algorithms/leetcode/easy/minimum-sum-of-four-digit.js
@@ -0,0 +1,24 @@
+/* 
+You are given a positive integer num consisting of exactly four digits. 
+Split num into two new integers new1 and new2 by using the digits found in num. 
+Leading zeros are allowed in new1 and new2, and all the digits found in num must be used.
+
+For example, given num = 2932, you have the following digits: two 2's, one 9 and one 3. 
+Some of the possible pairs [new1, new2] are [22, 93], [23, 92], [223, 9] and [2, 329].
+Return the minimum possible sum of new1 and new2.
+*/
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var minimumSum = function (num) {
+  const convertedNum = String(num)
+    .split("")
+    .sort((a, b) => a - b);
+
+  return (
+    Number(convertedNum[0] + convertedNum[2]) +
+    Number(convertedNum[1] + convertedNum[3])
+  );
+};
