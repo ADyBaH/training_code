83359f55c4e21bceff839afcdb348796f77785c0
Andrey N <brandon4@mail.ru>
2024-03-02 12:37:24 +0300

    feat: add solution for truncate-sentence.js

---
+++ type-challenge/easy/awaited.ts
@@ -0,0 +1,50 @@
+/*
+  189 - Awaited
+  -------
+  by Maciej Sikora (@maciejsikora) #easy #promise #built-in
+
+  ### Question
+
+  If we have a type which is a wrapped type like Promise, how we can get the type which is inside the wrapped type?
+
+  For example: if we have `Promise<ExampleType>` how to get ExampleType?
+
+  ```ts
+  type ExampleType = Promise<string>
+
+  type Result = MyAwaited<ExampleType> // string
+  ```
+
+  > This question is ported from the [original article](https://dev.to/macsikora/advanced-typescript-exercises-question-1-45k4) by [@maciejsikora](https://github.com/maciejsikora)
+
+  > View on GitHub: https://tsch.js.org/189
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type MyAwaited<T extends PromiseLike<any>> = T extends PromiseLike<infer U>
+  ? U extends PromiseLike<any>
+    ? MyAwaited<U>
+    : U
+  : never;
+
+/* _____________ Test Cases _____________ */
+
+type X = Promise<string>;
+type Y = Promise<{ field: number }>;
+type Z = Promise<Promise<string | number>>;
+type Z1 = Promise<Promise<Promise<string | boolean>>>;
+type T = { then: (onfulfilled: (arg: number) => any) => any };
+
+type MyAwaitedCase1 = MyAwaited<X>; //, string>
+type MyAwaitedCase2 = MyAwaited<Y>; //, { field: number }>
+type MyAwaitedCase3 = MyAwaited<Z>; //, string | number>
+type MyAwaitedCase4 = MyAwaited<Z1>; //, string | boolean>
+type MyAwaitedCase5 = MyAwaited<T>; //, number>
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/189/answer
+  > View solutions: https://tsch.js.org/189/solutions
+  > More Challenges: https://tsch.js.org
+*/
