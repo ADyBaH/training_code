67d278d28ce89b5b981f2da6ec8d6b411fb2ebd9
Andrey N <brandon4@mail.ru>
2024-03-23 08:53:31 +0300

    feat: add solution for minimum-operations.js

---
+++ algorithms/leetcode/easy/minimum-operations.js
@@ -0,0 +1,26 @@
+/* 
+You are given a 0-indexed integer array nums, and an integer k.
+
+In one operation, you can remove one occurrence of the smallest element of nums.
+
+Return the minimum number of operations needed so that all elements of the array are greater than or equal to k.
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums, k) {
+    let count = 0
+
+    for (let i = 0; i < nums.length; i++) {
+      if(nums[i] < k) {
+        count += 1
+      }
+    }
+
+    return count
+};
+
+console.log(minOperations([1,1,2,4,9], 9));
