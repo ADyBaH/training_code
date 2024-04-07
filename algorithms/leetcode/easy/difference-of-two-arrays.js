afb0b62e49d3e25fe1636589f3388df2698c662d
Andrey N <brandon4@mail.ru>
2024-04-07 08:23:51 +0300

    feat: add solution for difference-of-two-arrays.js

---
+++ algorithms/leetcode/easy/difference-of-two-arrays.js
@@ -0,0 +1,19 @@
+/* 
+Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+Note that the integers in the lists may be returned in any order.
+*/
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function (nums1, nums2) {
+  return [
+    [...new Set(nums1)].filter((number) => !nums2.includes(number)),
+    [...new Set(nums2)].filter((number) => !nums1.includes(number)),
+  ];
+};
